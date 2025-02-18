6036f10aebe19d58126cabf81b2abb5b81b3c8e2
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2025-02-18 14:50:07 +0530

    login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./screens/auth/auth.module').then(m => m.AuthModule) },
 
 ];
