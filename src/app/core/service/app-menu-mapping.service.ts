e242189af308e4c971d9f8439a8fd9cc6bb1b7cf
satya <satyadiverseinfotech@gmail.com>
2025-02-27 17:57:12 +0530

    integrated search api in app menu mapping

---
+++ src/app/core/service/app-menu-mapping.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { APIConstant } from '../constants/api.constant';
+import { AuthRequest } from '../models/auth';
+import { BaseService } from './base.service';
+import { CRUDService } from './crud.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppMenuMappingService extends CRUDService<AuthRequest> {
+  maxCount: number = Number.MAX_VALUE;
+
+  constructor(protected override baseService: BaseService) {
+    super(baseService, APIConstant.basePath);
+  }
+
+  appMenuMappingData(
+    userId: any,
+    offset: number = 0,
+    count: number = this.maxCount,
+    data: any
+  ) {
+    const url = `${APIConstant.appMenuMapping}/${userId}?offset=${offset}&count=${count}`;
+    return this.baseService.post(url, data);
+  }
+}
--- src/app/screens/masters/app-menu-mapping/filters/filters.component.ts
+++ src/app/screens/masters/app-menu-mapping/filters/filters.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrl: './filters.component.scss'
+  styleUrl: './filters.component.scss',
 })
 export class FiltersComponent {
+  @Input() filters: any;
 
+  appName: any;
+  menuName: any;
+  status: any;
+  @Output() getData: EventEmitter<object> =new EventEmitter();
+  
+
+  onSearch() {
+    let object = {
+      appName: this.appName || '',
+      menuName: this.menuName ||'',
+      status:this.status ||''
+    };
+    this.getData.emit(object)
+  }
+
+  onClear() {
+    this.appName=undefined;
+    this.menuName=undefined;
+    this.status=undefined;
+    let object = {
+      appName:undefined,
+      menuName:undefined,
+      status:undefined
+    }
+    this.getData.emit(object)
+  }
 }
