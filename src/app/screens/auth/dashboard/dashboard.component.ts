c8ea800c7f08deac4aeb0201902ae08a5b43d74d
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2025-03-05 16:13:29 +0530

    tiles view done if there is permission for more than one app

--- src/app/screens/auth/dashboard/dashboard.component.ts
+++ src/app/screens/auth/dashboard/dashboard.component.ts
@@ -8,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   apps: any;
+  accessToken: string = '';
 
   ngOnInit(): void {
     const storedData: any = localStorage.getItem('data');
     const dataObj = JSON.parse(storedData);
     this.apps = dataObj.apps;
+    this.accessToken = dataObj?.accessToken
+  }
+
+  onClickApp(data: any){
+    console.log(data);
+    
+    if(data.name.toLowerCase().replace(/\s/g, '') == 'usermanagamentsystem'){
+      window.location.href = data.route;
+    } else {
+      window.location.href = `${data.route}?data=${this.accessToken}&appId=${data.id}`;
+    }
     
   }
 
